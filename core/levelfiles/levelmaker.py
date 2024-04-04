d53b3475aa42de6bf0c437070db3c3e3dfa5cb95
Okistan <okistanpro@gmail.com>
2024-04-04 22:59:05 +0200

    Commencement module de parseur csv (venant de midicsv)

---
+++ core/levelfiles/levelmaker.py
@@ -0,0 +1,29 @@
+import sys
+
+csvfile = open(sys.argv[1], "r")
+
+elements = {}
+typeelement = ""
+
+precision = 0
+mstick = 0
+
+for line in csvfile:
+    channel, time, control = line[:-1].split(", ")[:3]
+    paramlist = line[:-1].split(", ")[3:]
+
+    if control == "Header":
+        precision = int(paramlist[2])
+    if control == "Tempo":
+        mstick = (int(paramlist[0])/1000)/precision
+    if control == "Title_t":
+        if "small" in paramlist[0]:
+            typeelement = "small"
+            elements[typeelement] = []
+    
+    if control == "Note_on_c" and typeelement != "long":
+        elements[typeelement].append(int(time)*mstick)
+
+print(elements)
+
+csvfile.close()
