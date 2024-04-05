b31fecbea7957e9310dda9afafdb971e075d97eb
Okistan <okistanpro@gmail.com>
2024-04-05 14:45:32 +0200

    Levelmaker peut maintenant fabriquer des niveaux de phase 1

--- core/levelfiles/levelmaker.py
+++ core/levelfiles/levelmaker.py
@@ -20,12 +20,55 @@ for line in csvfile:
         if "small" in paramlist[0]:
             typeelement = "small"
             elements[typeelement] = {"up" : [], "down" : []}
-    
-    if control == "Note_on_c" and typeelement != "long":
+        if "large" in paramlist[0]:
+            typeelement = "large"
+            elements[typeelement] = {"up" : [], "down" : []}
+        if "notes" in paramlist[0]:
+            typeelement = "notes"
+            elements[typeelement] = {"up" : [], "down" : []}
+        if "coeur" in paramlist[0]:
+            typeelement = "coeur"
+            elements[typeelement] = {"up" : [], "down" : []}
+        if "fantome" in paramlist[0]:
+            typeelement = "fantome"
+            elements[typeelement] = {"up" : [], "down" : []}
+        if "long" in paramlist[0]:
+            typeelement = "long"
+            elements[typeelement] = {"up" : [], "down" : []}
+            # "up" : [[depart, arrivé], [...]]
+        if "boss" in paramlist[0]:
+            typeelement = "boss"
+            elements[typeelement] = {"hit" : [], "long" : []}
+            # "hit" : [temps1, temps2, ...], "long" [[depart, arrivé], [...]]
+
+    if control == "Note_on_c" and typeelement != "long" and typeelement != "boss":
         if paramlist[1] == "61":
             elements[typeelement]["up"].append(int(time)*mstick)
         if paramlist[1] == "60":
             elements[typeelement]["down"].append(int(time)*mstick)
+    
+    if control == "Note_on_c" and typeelement == "long":
+        if paramlist[1] == "61":
+            elements[typeelement]["up"].append([int(time)*mstick])
+        if paramlist[1] == "60":
+            elements[typeelement]["down"].append([int(time)*mstick])
+    
+    if control == "Note_off_c" and typeelement == "long":
+        if paramlist[1] == "61":
+            elements[typeelement]["up"][-1].append(int(time)*mstick)
+        if paramlist[1] == "60":
+            elements[typeelement]["down"][-1].append(int(time)*mstick)
+
+    if control == "Note_on_c" and typeelement == "boss":
+        if paramlist[1] == "61":
+            elements[typeelement]["long"].append([int(time)*mstick])
+        if paramlist[1] == "60":
+            elements[typeelement]["hit"].append(int(time)*mstick)
+
+    if control == "Note_off_c" and typeelement == "boss":
+        if paramlist[1] == "61":
+            elements[typeelement]["long"][-1].append(int(time)*mstick)
+
 
 print(elements)
 
