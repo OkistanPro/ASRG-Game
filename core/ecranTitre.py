dcf0bd09c06f431e69edae0af0adaf9e9388bf7d
Maihry <auroreminihot@gmail.com>
2024-04-15 15:50:03 +0200

    Ajout fond écran titre

---
+++ core/ecranTitre.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+from pathlib import PurePath
+import levelfiles.levelmaker as levelmaker
+from classes import *
+
+import game
+
+import time
+
+import copy
+
+camera = [0, 0]
+
+fond = (0, 0, 0)
+
+objects = {"fond_ecranTITRE" : Actif(
+    {"anim1" : [PurePath("images/interface/fond_ecranTITRE.png")]},
+    {"anim1" : [False, 5]},
+    "anim1"
+)}
