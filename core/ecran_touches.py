54480b68cc962f25e041a54b03e2deb6bf9d5108
Okistan <okistanpro@gmail.com>
2024-05-01 16:19:58 +0200

    Bug touches : mauvaise condition réglé

--- core/ecran_touches.py
+++ core/ecran_touches.py
@@ -479,7 +479,7 @@ def loopevent(event):
         objects["choixtxt"].visible = True
 
     if event.type == KEYDOWN and objects["touche_bas1"].animCourante == "touche_bas1b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["bas"][0] = event.key
         objects["touche_bas1txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_bas1txt"][0] = 665-(objects["touche_bas1txt"].renderText().get_rect().width/2)
@@ -487,9 +487,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
     if event.type == KEYDOWN and objects["touche_bas2"].animCourante == "touche_bas2b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["bas"][1] = event.key
         objects["touche_bas2txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_bas2txt"][0] = 738-(objects["touche_bas2txt"].renderText().get_rect().width/2)
@@ -497,9 +498,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
     if event.type == KEYDOWN and objects["touche_bas3"].animCourante == "touche_bas3b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["bas"][2] = event.key
         objects["touche_bas3txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_bas3txt"][0] = 811-(objects["touche_bas3txt"].renderText().get_rect().width/2)
@@ -507,9 +509,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
     
     if event.type == KEYDOWN and objects["touche_haut1"].animCourante == "touche_haut1b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["haut"][0] = event.key
         objects["touche_haut1txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_haut1txt"][0] = 415-(objects["touche_haut1txt"].renderText().get_rect().width/2)
@@ -517,9 +520,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
     if event.type == KEYDOWN and objects["touche_haut2"].animCourante == "touche_haut2b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["haut"][1] = event.key
         objects["touche_haut2txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_haut2txt"][0] = 340-(objects["touche_haut2txt"].renderText().get_rect().width/2)
@@ -527,9 +531,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
     if event.type == KEYDOWN and objects["touche_haut3"].animCourante == "touche_haut3b" and ((event.key in range(97,123)) or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT or not(event.key in game.boutons["bas"] or event.key in game.boutons["haut"])):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["haut"][2] = event.key
         objects["touche_haut3txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_haut3txt"][0] = 490-(objects["touche_haut3txt"].renderText().get_rect().width/2)
@@ -537,9 +542,10 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
     if event.type == KEYDOWN and objects["touche_phase3"].animCourante == "touche_phase3b" and ((event.key in range(97,123)) or event.key==K_SPACE or event.key==K_UP or event.key==K_DOWN\
-        or event.key==K_LEFT or event.key==K_RIGHT):
+        or event.key==K_LEFT or event.key==K_RIGHT) and not (event.key in game.boutons["bas"] or event.key in game.boutons["haut"]):
         game.boutons["saut"] = event.key
         objects["touche_phase3txt"].changeTexte(pygame.key.name(event.key).upper())
         calques[1]["touche_phase3txt"][0] = 576-(objects["touche_phase3txt"].renderText().get_rect().width/2)
@@ -547,6 +553,7 @@ def loopevent(event):
         objects["fondgris"].visible = False
         objects["choix"].visible = False
         objects["choixtxt"].visible = False
+        print(game.boutons)
 
 def loopbeforeupdate():
     pass
